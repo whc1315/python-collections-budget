2eb22235c623a65dec519478c1685749bd02fd41
William Cuthbertson <whc1315@gmail.com>
2022-03-08 09:31:20 -0700

    Mod2

---
+++ budget/BudgetList.py
@@ -0,0 +1,10 @@
+class BudgetList:
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def append(sefl, item):
+        pass
