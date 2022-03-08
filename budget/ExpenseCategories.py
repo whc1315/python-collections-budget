ed99a24a744cbddab0195389b531118f44f5eff2
William Cuthbertson <whc1315@gmail.com>
2022-03-08 11:13:22 -0700

    mod4.12

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,10 +1,14 @@
 from . import Expense
 import matplotlib.pyplot as plt
 
+
 def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
+    divided_set_comp = expenses.categorize_set_comprehension()
+    if divided_for_loop != divided_set_comp:
+        print('Sets are NOT equal by == test')
 
 
 if __name__ == "__main__":
