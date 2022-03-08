e6fe318e9b989193e22bc7d86de4259d33bfc21d
William Cuthbertson <whc1315@gmail.com>
2022-03-08 13:12:17 -0700

    mod5.15

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -21,6 +21,14 @@ def main():
                   ''',
                         number=100000,
                         globals=globals()))
+    print(timeit.timeit(stmt="expenses.categorize_set_comprehension()",
+                        setup='''
+                  from . import Expense
+                  expenses = Expense.Expenses()
+                  expenses.read_expenses('data/spending_data.csv')
+                  ''',
+                        number=100000,
+                        globals=globals()))
 
 
 if __name__ == "__main__":
